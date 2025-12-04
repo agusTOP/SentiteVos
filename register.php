c9874f2a40a3df7ccd0e051f78171a2271b95895
agusTOP <agu_saquilano@hotmail.com>
2025-12-04 19:42:24 -0300

    Se agrega los archivos de bootstrap

--- register.php
+++ register.php
@@ -8,7 +8,8 @@ ensureCsrfToken(); ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Registrarse - Sentite Vos</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles/bootstrap.min.css">
+  <script src="scripts/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="styles/styles.css">
   <link rel="icon" href="assets/icon.ico">
 </head>
